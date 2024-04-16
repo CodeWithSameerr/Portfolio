f90f12ad2fe8ef863b07200d1e9c1e50f9d220df
Tec-Sam <154794709+Tec-Sam@users.noreply.github.com>
2024-04-16 13:18:54 +0500

    Add files via upload

---
+++ asset/js/viewcv.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    $('#viewCvBtn').click(function(){
+      // Replace 'your-cv-url.pdf' with the actual URL of your CV PDF file
+      window.open('Sam_Resume.pdf', '_blank');
+    });
+  });
